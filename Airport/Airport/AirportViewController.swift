a5a20867a371058f1f94901f28038ce99757cedb
chriswebb09 <christopher.webb@flatironschool.com>
2017-10-14 23:43:12 -0400

    initial commit

--- Airport/Airport/AirportViewController.swift
+++ Airport/Airport/AirportViewController.swift
@@ -21,8 +21,21 @@ class AirportViewController: UIViewController {
     @IBOutlet weak var sceneView: PlaneSceneView!
     
     var locationService = LocationService()
+    var navigationService: NavigationService = NavigationService()
+    private var annotationColor = UIColor.blue
+    internal var annotations: [POIAnnotation] = []
+    
+    private var currentTripLegs: [[CLLocationCoordinate2D]] = []
+    private var steps: [MKRouteStep] = []
+    private var locations: [CLLocation] = []
     var startingLocation: CLLocation!
-    var destinationLocation: CLLocation!
+    
+    var destinationLocation: CLLocation! {
+        didSet {
+            setupNavigation()
+        }
+    }
+    var press: UILongPressGestureRecognizer!
     var airportPlaced: Bool = false
     var planeCanFly: Bool = false
     var mapDismissed: Bool = false
@@ -31,11 +44,15 @@ class AirportViewController: UIViewController {
         super.viewDidLoad()
         runSession()
         sceneView.setupScene()
-        destinationLocation = CLLocation(latitude: 40.737359, longitude: -73.979086)
+      //  destinationLocation = CLLocation(latitude: 40.737359, longitude: -73.979086)
         locationService.delegate = self
         sceneView.setupAirport()
         sceneView.setupPlane()
         locationService.startUpdatingLocation(locationManager: locationService.locationManager!)
+        press = UILongPressGestureRecognizer(target: self, action: #selector(handleMapTap(gesture:)))
+        press.minimumPressDuration = 0.35
+        mapView.addGestureRecognizer(press)
+        mapView.delegate = self
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -116,10 +133,154 @@ extension AirportViewController: LocationServiceDelegate {
     
     func trackingLocation(for currentLocation: CLLocation) {
         startingLocation = currentLocation
-        planeCanFly = true
+        centerMapInInitialCoordinates()
     }
     
     func trackingLocationDidFail(with error: Error) {
         print(error.localizedDescription)
+    }
+}
+
+extension AirportViewController: Mapable {
+    
+    @objc func handleMapTap(gesture: UIGestureRecognizer) {
+        if gesture.state != UIGestureRecognizerState.began {
+            return
+        }
+        // Get tap point on map
+        let touchPoint = gesture.location(in: mapView)
+        
+        // Convert map tap point to coordinate
+        let coord: CLLocationCoordinate2D = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        destinationLocation = CLLocation(latitude: coord.latitude, longitude: coord.longitude)
+        planeCanFly = true 
+    }
+}
+
+extension AirportViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
+        annotationView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        annotationView.canShowCallout = true
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+            let renderer = MKCircleRenderer(overlay: overlay)
+            renderer.fillColor = UIColor.black.withAlphaComponent(0.1)
+            renderer.strokeColor = annotationColor
+            renderer.lineWidth = 2
+            return renderer
+        }
+        return MKOverlayRenderer()
+    }
+    
+    private func setupNavigation() {
+        
+        let group = DispatchGroup()
+        group.enter()
+        
+        DispatchQueue.global(qos: .default).async {
+            
+            if self.destinationLocation != nil {
+                self.navigationService.getDirections(destinationLocation: self.destinationLocation.coordinate, request: MKDirectionsRequest()) { steps in
+                    for step in steps {
+                        self.annotations.append(POIAnnotation(coordinate: step.getLocation().coordinate, name: "N " + step.instructions))
+                    }
+                    self.steps.append(contentsOf: steps)
+                    group.leave()
+                }
+            }
+            
+            // All steps must be added before moving to next step
+            group.wait()
+            
+            self.getLocationData()
+        }
+    }
+    
+    private func getLocationData() {
+        
+        for (index, step) in steps.enumerated() {
+            setTripLegFromStep(step, and: index)
+        }
+        
+        for leg in currentTripLegs {
+            update(intermediary: leg)
+        }
+        
+        centerMapInInitialCoordinates()
+        showPointsOfInterestInMap(currentTripLegs: currentTripLegs)
+        addMapAnnotations()
+    }
+    
+    // Gets coordinates between two locations at set intervals
+    private func setLeg(from previous: CLLocation, to next: CLLocation) -> [CLLocationCoordinate2D] {
+        return CLLocationCoordinate2D.getIntermediaryLocations(currentLocation: previous, destinationLocation: next)
+    }
+    
+    // Add POI dots to map
+    private func showPointsOfInterestInMap(currentTripLegs: [[CLLocationCoordinate2D]]) {
+        mapView.removeAnnotations(mapView.annotations)
+        for tripLeg in currentTripLegs {
+            for coordinate in tripLeg {
+                let poi = POIAnnotation(coordinate: coordinate, name: String(describing: coordinate))
+                mapView.addAnnotation(poi)
+            }
+        }
+    }
+    
+    // Adds calculated distances to annotations and locations arrays
+    private func update(intermediary locations: [CLLocationCoordinate2D]) {
+        for intermediaryLocation in locations {
+            annotations.append(POIAnnotation(coordinate: intermediaryLocation, name: String(describing:intermediaryLocation)))
+            self.locations.append(CLLocation(latitude: intermediaryLocation.latitude, longitude: intermediaryLocation.longitude))
+        }
+    }
+    
+    // Determines whether leg is first leg or not and routes logic accordingly
+    private func setTripLegFromStep(_ tripStep: MKRouteStep, and index: Int) {
+        if index > 0 {
+            getTripLeg(for: index, and: tripStep)
+        } else {
+            getInitialLeg(for: tripStep)
+        }
+    }
+    
+    // Calculates intermediary coordinates for route step that is not first
+    private func getTripLeg(for index: Int, and tripStep: MKRouteStep) {
+        let previousIndex = index - 1
+        let previousStep = steps[previousIndex]
+        let previousLocation = CLLocation(latitude: previousStep.polyline.coordinate.latitude, longitude: previousStep.polyline.coordinate.longitude)
+        let nextLocation = CLLocation(latitude: tripStep.polyline.coordinate.latitude, longitude: tripStep.polyline.coordinate.longitude)
+        let intermediarySteps = CLLocationCoordinate2D.getIntermediaryLocations(currentLocation: previousLocation, destinationLocation: nextLocation)
+        currentTripLegs.append(intermediarySteps)
+    }
+    
+    // Calculates intermediary coordinates for first route step
+    private func getInitialLeg(for tripStep: MKRouteStep) {
+        let nextLocation = CLLocation(latitude: tripStep.polyline.coordinate.latitude, longitude: tripStep.polyline.coordinate.longitude)
+        let intermediaries = CLLocationCoordinate2D.getIntermediaryLocations(currentLocation: startingLocation, destinationLocation: nextLocation)
+        currentTripLegs.append(intermediaries)
+    }
+    
+    // Prefix N is just a way to grab step annotations, could definitely get refactored
+    private func addMapAnnotations() {
+        
+        annotations.forEach { annotation in
+            
+            // Step annotations are green, intermediary are blue
+            DispatchQueue.main.async {
+                if let title = annotation.title, title.hasPrefix("N") {
+                    self.annotationColor = .green
+                } else {
+                    self.annotationColor = .blue
+                }
+                self.mapView?.addAnnotation(annotation)
+                self.mapView.add(MKCircle(center: annotation.coordinate, radius: 0.2))
+            }
+        }
     }
 }
--- Airport/Airport/MatrixHelper.swift
+++ Airport/Airport/MatrixHelper.swift
@@ -10,6 +10,21 @@ import Foundation
 import CoreLocation
 import SceneKit
 
+struct LocationConstants {
+    static let metersPerRadianLat: Double = 6373000.0
+    static let metersPerRadianLon: Double = 5602900.0
+}
+
+
+import MapKit
+
+// Get a CLLocation from a route step
+extension MKRouteStep {
+    func getLocation() -> CLLocation {
+        return CLLocation(latitude: polyline.coordinate.latitude, longitude: polyline.coordinate.longitude)
+    }
+}
+
 extension CLLocationCoordinate2D: Equatable {
     
     public static func ==(lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
@@ -28,6 +43,45 @@ extension CLLocationCoordinate2D: Equatable {
         let x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon)
         let radiansBearing = atan2(y, x)
         return radiansBearing
+    }
+    
+    func calculateDirection(to coordinate: CLLocationCoordinate2D) -> Double {
+        let a = sin(coordinate.longitude.toRadians() - longitude.toRadians()) * cos(coordinate.latitude.toRadians())
+        let dLat = cos(latitude.toRadians()) * sin(coordinate.latitude.toRadians()) - sin(latitude.toRadians())
+        let dLon = cos(coordinate.latitude.toRadians()) * cos(coordinate.longitude.toRadians() - longitude.toRadians())
+        let b =  dLat * dLon
+        return atan2(a, b)
+    }
+    
+    func direction(to coordinate: CLLocationCoordinate2D) -> CLLocationDirection {
+        return self.calculateDirection(to: coordinate).toDegrees()
+    }
+    
+ 
+    
+    func coordinate(with bearing: Double, and distance: Double) -> CLLocationCoordinate2D {
+        let distRadiansLat = distance / LocationConstants.metersPerRadianLat  // earth radius in meters latitude
+        let distRadiansLong = distance / LocationConstants.metersPerRadianLon // earth radius in meters longitude
+        let lat1 = self.latitude.toRadians()
+        let lon1 = self.longitude.toRadians()
+        let lat2 = asin(sin(lat1) * cos(distRadiansLat) + cos(lat1) * sin(distRadiansLat) * cos(bearing))
+        let lon2 = lon1 + atan2(sin(bearing) * sin(distRadiansLong) * cos(lat1), cos(distRadiansLong) - sin(lat1) * sin(lat2))
+        return CLLocationCoordinate2D(latitude: lat2.toDegrees(), longitude: lon2.toDegrees())
+    }
+    
+    static func getIntermediaryLocations(currentLocation: CLLocation, destinationLocation: CLLocation) -> [CLLocationCoordinate2D] {
+        var distances = [CLLocationCoordinate2D]()
+        let metersIntervalPerNode: Float = 10
+        var distance = Float(destinationLocation.distance(from: currentLocation))
+        let bearing = currentLocation.bearingToLocationRadian(destinationLocation)
+        while distance > 10 {
+            distance -= metersIntervalPerNode
+            let newLocation = currentLocation.coordinate.coordinate(with: Double(bearing), and: Double(distance))
+            if !distances.contains(newLocation) {
+                distances.append(newLocation)
+            }
+        }
+        return distances
     }
 }
 
